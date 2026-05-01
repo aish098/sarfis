b8c55db58733c0453c4d588293492a4092423746
SARFIS Developer <dev@sarfis.io>
2026-05-01 06:52:43 -0700

    Debug: Added DB connection logging for production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,13 @@ const knex = require('knex');
 const knexConfig = require('../../knexfile');
 
 const environment = process.env.NODE_ENV || 'development';
+console.log(`[DB] Initializing in ${environment} mode...`);
 const db = knex(knexConfig[environment]);
+
+db.raw('SELECT 1').then(() => {
+  console.log(`[DB] Successfully connected to database.`);
+}).catch(err => {
+  console.error(`[DB] Connection FAILED:`, err.message);
+});
 
 module.exports = db;
