c8870c6e5894965a4d9ed928a537da0afa2d9492
SARFIS Developer <dev@sarfis.io>
2026-05-02 06:08:08 -0700

    Cleanup: Suppressed persistent motion lint warning in LeadershipPage

--- frontend/src/pages/LeadershipPage.jsx
+++ frontend/src/pages/LeadershipPage.jsx
@@ -14,6 +14,7 @@
  */
 
 import { useEffect, useRef, useState } from "react";
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
